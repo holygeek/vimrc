b626c64b958bb0fb35f814dacd50856f72b2ebda
Nazri Ramliy <ayiehere@gmail.com>
2023-08-25 07:44:41 +0800

    add getopt.sh

---
+++ bin/getopt.sh
@@ -0,0 +1,20 @@
+#!/bin/sh
+
+me=$(basename $0)
+
+usage() {
+	echo "usage: $me [-h] ...
+  Options:
+    -h  Help"
+}
+
+foo=
+while getopts hf: opt
+do
+  case "$opt" in
+    f) foo=$OPTARG;;
+    h) usage; exit ;;
+    \?) echo Unknown option ; exit;;
+  esac
+done
+shift $(($OPTIND -1))

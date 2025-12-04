76c295ac086f93f1d4fd8f32b2a9b10431a8a52e
Nazri Ramliy <ayiehere@gmail.com>
2025-12-04 17:42:07 +0800

    getopt.sh: formatting

--- bin/getopt.sh
+++ bin/getopt.sh
@@ -4,17 +4,18 @@ me=$(basename $0)
 
 usage() {
 	echo "usage: $me [-h] ...
-  Options:
-    -h  Help"
+Options:
+	-h
+	  Help"
 }
 
 foo=
 while getopts hf: opt
 do
-  case "$opt" in
-    f) foo=$OPTARG;;
-    h) usage; exit ;;
-    \?) echo Unknown option ; exit;;
-  esac
+	case "$opt" in
+		f) foo=$OPTARG;;
+		h) usage; exit ;;
+		\?) echo Unknown option ; exit;;
+	esac
 done
 shift $(($OPTIND -1))
